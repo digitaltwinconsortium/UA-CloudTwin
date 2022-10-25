94595831a47b90f7bf19d969841b6eafdc3ebdd5
Erich Barnstedt <erichb@microsoft.com>
2022-10-25 20:42:51 +0200

    renamed file

---
+++ Applications/Interfaces/IMessageProcessor.cs
@@ -0,0 +1,12 @@
+﻿
+namespace UACloudTwin.Interfaces
+{
+    using System;
+
+    public interface IMessageProcessor
+    {
+        void Clear();
+
+        void ProcessMessage(byte[] payload, DateTime receivedTime, string contentType);
+    }
+}
